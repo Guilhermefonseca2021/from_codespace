7540c83568f6746d7412efe25d51723d6c4e6396
Guilherme Fonseca Dos Santos <92196697+Guilhermefonseca2021@users.noreply.github.com>
2026-03-11 15:04:16 +0000

    first setup initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # from_codespace
+
 a test repositorie direct from codespace of github, games lirary in next.
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-purple-600">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  )
+}
